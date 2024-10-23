1c06bdfcc328526960c0f3931abe851f8a980f4a
altuga <altuga@gmail.com>
2024-10-23 15:40:53 +0300

    ready for Kata

--- src/test/java/org/jugistanbul/CalculatorServiceTest.java
+++ src/test/java/org/jugistanbul/CalculatorServiceTest.java
@@ -5,32 +5,6 @@ import org.junit.Test;
 
 public class CalculatorServiceTest {
 
-    @Test
-    public void testAdd() {
-        CalculatorService calculatorService = new CalculatorService();
-        double result = calculatorService.add(1d, 1d);
-        Assert.assertEquals(result, 2d, 0d);
-    }
 
-    @Test
-    public void testSub() {
-        CalculatorService calculatorService= new CalculatorService();
-        double result = calculatorService.sub(5d,3d);
-        Assert.assertEquals(result, 2d, 0d);
-    }
-
-    @Test
-    public void testMultiply() {
-        CalculatorService calculatorService = new CalculatorService();
-        double result = calculatorService.mul(5d, 2d);
-        Assert.assertEquals(result, 10, 0d);
-    }
-
-    @Test
-    public void testDivide() {
-        CalculatorService calculatorService = new CalculatorService();
-        double result = calculatorService.div(10d, 5d);
-        Assert.assertEquals(result, 2, 0d);
-    }
 
 }
