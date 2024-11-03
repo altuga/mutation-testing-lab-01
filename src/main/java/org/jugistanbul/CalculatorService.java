918ef37927cec303a373363cde6dc9d90996a1fa
altuga <altuga@gmail.com>
2024-11-03 14:11:27 +0300

    name of the artifact has changed

--- src/main/java/org/jugistanbul/CalculatorService.java
+++ src/main/java/org/jugistanbul/CalculatorService.java
@@ -1,12 +1,14 @@
 package org.jugistanbul;
 
+import java.math.BigDecimal;
+
 /**
  * Hello world!
  *
  */
 public class CalculatorService  {
 
-    public double add (Double number1, Double number2) {
+    public Double add (Double number1, Double number2) {
         return number1 + number2;
     }
 
--- src/test/java/org/jugistanbul/CalculatorServiceTest.java
+++ src/test/java/org/jugistanbul/CalculatorServiceTest.java
@@ -1,9 +1,45 @@
 package org.jugistanbul;
 
-import org.junit.Assert;
+
 import org.junit.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
 public class CalculatorServiceTest {
+
+    @Test
+    public void testAdd() {
+        CalculatorService calculatorService = new CalculatorService();
+        double value = calculatorService.add(1d,1d);
+        assertEquals(2d, value);
+    }
+
+    @Test
+    public void testSub() {
+        CalculatorService calculatorService = new CalculatorService();
+        double value = calculatorService.sub(10d, 1d);
+        assertEquals(9d, value);
+
+    }
+
+    @Test
+    public void testDiv() {
+        CalculatorService calculatorService = new CalculatorService();
+        double value = calculatorService.div(10d, 2d);
+        assertEquals(5d,value);
+
+    }
+
+    @Test
+    public void testMul() {
+        CalculatorService calculatorService = new CalculatorService();
+        double value = calculatorService.mul(5d, 2d);
+        assertEquals(10d, value);
+
+    }
+
 
 
 
