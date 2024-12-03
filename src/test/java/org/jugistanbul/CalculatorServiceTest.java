55e243778c5c106d6601f97070a6ff4e5306639c
altuga <altuga@gmail.com>
2024-12-03 10:37:13 +0300

    clean up

--- src/test/java/org/jugistanbul/CalculatorServiceTest.java
+++ src/test/java/org/jugistanbul/CalculatorServiceTest.java
@@ -10,27 +10,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorServiceTest {
 
-    @Test
-    public void testAdd(){
-        CalculatorService calculatorService = new CalculatorService();
-        double result = calculatorService.add(1d, 1d);
-        Assert.assertEquals(2, result, 0d);
-    }
-
-    @Test
-    public void testDiv(){
-        CalculatorService calculatorService = new CalculatorService();
-        double result = calculatorService.div(1d, 1d);
-        Assert.assertEquals(1, result, 0d);
-    }
-
-    @Test
-    public void testMul(){
-        CalculatorService calculatorService = new CalculatorService();
-        double result = calculatorService.mul(10d, 1d);
-        Assert.assertEquals(10, result, 0d);
-    }
-
 
 
 
